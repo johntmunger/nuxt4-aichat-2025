5af65c6543ef610e7a9c4b20a0bc598f260f4396
johntmunger <johntmunger@gmail.com>
2025-09-30 09:01:35 -0600

    nuxt with packaged ui integration

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">
+      Welcome to Nuxt UI
+    </h1>
+    <p class="mb-2">
+      This is the home page of your Nuxt UI application.
+    </p>
+    <nuxt-link to="/about" class="text-blue-500 hover:underline">
+      Go to About Page
+    </nuxt-link>
+  </div>
+</template>

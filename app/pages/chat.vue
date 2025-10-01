e60f490b723a8137e0d578e72bd7d0cf240bcc09
johntmunger <johntmunger@gmail.com>
2025-10-01 13:32:01 -0600

    create some chat mock data with type interface and a composable

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>This is a chat</h1>
+  <div class="layout-container">
+    <h1>This is a chat</h1>
+  </div>
 </template>
+
+<style scoped>
+  .layout-container {
+    background-color: var(--background-color-primary);
+  }
+</style>
